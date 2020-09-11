0101cb4d08c8ee35483631b4f5b90fc549149ccc
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-11 19:43:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,13 +3,15 @@ require 'pry'
 def game_hash 
 {
   :home => {
-    :team_name
+    :team_name =
     :colors 
     :players 
     
   }
   :away => {
-    
+    :team_name
+    :colors 
+    :players 
   }
 }
 end 
