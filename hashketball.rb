20d4a8bab6cbe2b284387f87c861ee556e09097e
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-11 20:12:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,8 +16,7 @@ def game_hash
         :steals => 3,
         :blocks => 1 
         :slam_dunks => 1 
-    },
-    binding.pry 
+      },
     {
        :player_name
         :number
@@ -60,6 +59,7 @@ def game_hash
         :steals
         :blocks
         :slam_dunks
+    }
   },
   :away => {
     :team_name => "Charlotte Hornets",
