798cabce7a7943df55c9533763eb2442b4caa818
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-11 20:48:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@
 require 'pry' 
 def game_hash 
 {
-  :home => {:team_name => "Brooklyn Nets",},
-  :away => {}
+  :home => {:team_name => "Brooklyn Nets"},
+  :away => {:team_name => "Charlotte Hornets"}
 }
 end 
