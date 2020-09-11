5b11395f0d9f342975bdf723dc8586435964edd6
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-11 19:31:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,6 @@
 # Write your code here!
 require 'pry' 
-def game_hash (hasketball)
- hasketball.each_with_object do { |hash, key| hash[key]}
-  if !hasketball[:home]
-    hasketball[:home] = {}
-  end 
- end 
+def game_hash 
+ {}
+ binding.pry
 end 
