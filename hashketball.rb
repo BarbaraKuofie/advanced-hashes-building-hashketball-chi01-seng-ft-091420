fe72d870dda05221cb840ed95f7917f9026f0164
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-11 19:46:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,14 +4,14 @@ def game_hash
 {
   :home => {
     :team_name => "Brooklyn Nets"
-    :colors => [ ]
+    :colors => [ "Black", "White"]
     :players 
     
   }
   :away => {
     :team_name => "Charlotte Hornets"
-    :colors 
-    :players 
+    :colors => ["Turquoise", "Purple"]
+    :players => [{}]
   }
 }
 end 
