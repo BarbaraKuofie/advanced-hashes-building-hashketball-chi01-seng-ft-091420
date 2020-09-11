78ca5e7b4a886b962812ecaf3ac4f775c3452688
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-11 20:23:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,66 @@ require 'pry'
 def game_hash 
 {
   :home => { 
-    :team_name
-    :colors 
-    :players
+    "Brooklyn Nets",
+    :colors => [ "Black", "White"],
+    :players => [
+      {
+        :player_name => "Alan Anderson",  
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1 
+        binding.pry
+      },
+    {
+       :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+    },
+    {
+       :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+    },
+    {
+       :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+    },
+    { :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+    }
+  ],
+
   },
   :away => { 
     :team_name 
